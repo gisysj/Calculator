5ec6aee6d3e9f8f0b0e2f39dc01a51204ccd405e
gisysj <gisysj@163.com>
2018-02-04 23:03:05 +0800

    Add files via upload

---
+++ Calculator.h
@@ -0,0 +1,14 @@
+//
+//  Calculator.h
+//  Calculator
+//
+//  Created by ysj on 2018/2/4.
+//  Copyright © 2018年 ysj. All rights reserved.
+//
+
+#ifndef Calculator_h
+#define Calculator_h
+
+int Add(int x, int y);
+
+#endif /* Calculator_h */
