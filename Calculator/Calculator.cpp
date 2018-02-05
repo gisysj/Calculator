1d8ffacf9216c3757bea95b265320d4b0be686be
siji.ysj <siji.ysj@alibaba-inc.com>
2018-02-05 22:38:25 +0800

    添加文件夹和代码

---
+++ Calculator/Calculator.cpp
@@ -0,0 +1,14 @@
+//
+//  Calculator.cpp
+//  Calculator
+//
+//  Created by ysj on 2018/2/4.
+//  Copyright © 2018年 ysj. All rights reserved.
+//
+
+#include <stdio.h>
+
+int Add(int x, int y)
+{
+    return x + y;
+}
---
+++ Calculator/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  Calculator
+//
+//  Created by ysj on 2018/2/4.
+//  Copyright © 2018年 ysj. All rights reserved.
+//
+
+#include <iostream>
+#include "Calculator.h"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    //std::cout << "Hello, World!\n";
+    
+    
+    //1）测试加法
+    int nSum = Add(3, 5);
+    
+    return 0;
+}
